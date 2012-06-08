91809cc0256e5e762955e2d5d896346c32355dbc
Vijay <vijayb@gmail.com>
2012-06-08 11:05:23 -0700

    Add opticrop function to helpers

--- www/helpers.php
+++ www/helpers.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL | E_STRICT);
 $s3_root = "https://s3.amazonaws.com/zipio_photos";
 $www_root = "http://localhost";
 //$www_root = "http://" . $_SERVER["HTTP_HOST"];
-
+define('CACHE_PATH', 'opticrop-cache/');
 
 function rand_string($length) {
     $chars = "abcdefghijklmnopqrstuvwxyz";
@@ -55,10 +55,12 @@ function send_email($to, $from, $subject, $html) {
         $from = "Zipio <founders@zipio.com>";
     }
 
-    $request = new HttpRequest('https://api.mailgun.net/v2/zipio.com/messages', HttpRequest::METH_POST);
+    $request = new HttpRequest('https://api.mailgun.net/v2/zipio.com/messages',
+                               HttpRequest::METH_POST);
     $auth = base64_encode('api:key-68imhgvpoa-6uw3cl8728kcs9brvlmr9');
     $request->setHeaders(array('Authorization' => 'Basic '.$auth));
-    $request->setPostFields(array('from' => $from, 'to' => $to, 'subject' => $subject, 'html' => $html));
+    $request->setPostFields(array('from' => $from, 'to' => $to,
+                                  'subject' => $subject, 'html' => $html));
     $request->send();
 
     return $request;
@@ -122,7 +124,8 @@ function is_friend($user_id, $potential_friend_id) {
 
     $query = "SELECT * FROM Friends WHERE user_id='$user_id' AND friend_id=$potential_friend_id";
     $result = mysql_query($query, $con);
-    if (!$result) die('Invalid query in ' . __FUNCTION__ . ': ' . mysql_error());
+    if (!$result)
+        die('Invalid query in ' . __FUNCTION__ . ': ' . mysql_error());
 
     if (mysql_num_rows($result) == 1) {
         return 1;
@@ -158,7 +161,8 @@ function create_album($user_id, $handle) {
                   '$handle'
               ) ON DUPLICATE KEY UPDATE id=id";
     $result = mysql_query($query, $con);
-    if (!$result) die('Invalid query in ' . __FUNCTION__ . ': ' . $query . " - " . mysql_error());
+    if (!$result) die('Invalid query in ' . __FUNCTION__ .
+                      ': ' . $query . " - " . mysql_error());
 
     $album_id = mysql_insert_id();
     return $album_id;
@@ -178,7 +182,8 @@ function add_photo($owner_user_id, $target_album_id, $target_album_owner_id,
     if (!$visible) $visible_string = "<b>invisible</b>";
     debug("Adding $visible_string photo (owner $owner_user_id) to album $target_album_id");
 
-    $s3_url = $owner_user_id ."_". $target_album_id . "_" . sha1(rand_string(20));
+    $s3_url =
+        $owner_user_id ."_". $target_album_id . "_" . sha1(rand_string(20));
     $bucket_name = "zipio_photos";
 
     $sizes = array(800, 480, 240);
@@ -197,7 +202,8 @@ function add_photo($owner_user_id, $target_album_id, $target_album_owner_id,
 
         $s3_name = $s3_url . "_" . $sizes[$ii];
 
-        if (!$s3->putObjectFile($tmp_path_to_photo, $bucket_name, $s3_name, S3::ACL_PUBLIC_READ)) {
+        if (!$s3->putObjectFile($tmp_path_to_photo, $bucket_name,
+                                $s3_name, S3::ACL_PUBLIC_READ)) {
             debug("Error in writing to S3");
             debug($tmp_path_to_photo);
             $failed = 1;
@@ -217,7 +223,8 @@ function add_photo($owner_user_id, $target_album_id, $target_album_owner_id,
                     '$s3_url'
                   ) ON DUPLICATE KEY UPDATE id=id";
         $result = mysql_query($query, $con);
-        if (!$result) die('Invalid query in ' . __FUNCTION__ . ': ' . $query . " - " . mysql_error());
+        if (!$result) die('Invalid query in ' . __FUNCTION__ . ': ' .
+                          $query . " - " . mysql_error());
         $photo_id = mysql_insert_id();
 
         $query = "INSERT INTO AlbumPhotos (
@@ -234,7 +241,8 @@ function add_photo($owner_user_id, $target_album_id, $target_album_owner_id,
                     '$visible'
                   ) ON DUPLICATE KEY UPDATE id=id";
         $result = mysql_query($query, $con);
-        if (!$result) die('Invalid query in ' . __FUNCTION__ . ': ' . $query . " - " . mysql_error());
+        if (!$result) die('Invalid query in ' . __FUNCTION__ . ': ' .
+                          $query . " - " . mysql_error());
 
         return $photo_id;
 
@@ -410,8 +418,121 @@ function update_data($table, $id, $key_values) {
 
 
 
+// Input is an imagemagick image. $w/$h are the dimensions of the desired
+// cropped image, and $out is where the cropped image will be written.
+// Note that the dimensions of $image must be greater than or equal to
+// $w x $h in both dimensions.
+function opticrop($img, $w, $h, $out) {
+    // source dimensions
+    $w0 = $img->getImageWidth();
+    $h0 = $img->getImageHeight();
+    if ($w > $w0 || $h > $h0)
+        die("Target dimensions must be smaller or equal to source dimensions.");
 
+    // parameters for the edge-maximizing crop algorithm
+    $r = 1;         // radius of edge filter
+    $nk = 9;        // scale count: number of crop sizes to try
+    $gamma = 0.2;   // edge normalization parameter -- see documentation
+    $ar = $w/$h;    // target aspect ratio (AR)
+    $ar0 = $w0/$h0;    // target aspect ratio (AR)
 
+    //    dprint("$image: $w0 x $h0 => $w x $h");
+    $imgcp = clone $img;
+
+    // compute center of edginess
+    $img->edgeImage($r);
+    $img->modulateImage(100,0,100); // grayscale
+    $img->blackThresholdImage("#0f0f0f");
+    $img->writeImage($out);
+    // use gd for random pixel access
+    $im = ImageCreateFromJpeg($out);
+    $xcenter = 0;
+    $ycenter = 0;
+    $sum = 0;
+    $n = 100000;
+    for ($k=0; $k<$n; $k++) {
+        $i = mt_rand(0,$w0-1);
+        $j = mt_rand(0,$h0-1);
+        $val = imagecolorat($im, $i, $j) & 0xFF;
+        $sum += $val;
+        $xcenter += ($i+1)*$val;
+        $ycenter += ($j+1)*$val;
+    }
+    $xcenter /= $sum;
+    $ycenter /= $sum;
+
+    // crop source img to target AR
+    if ($w0/$h0 > $ar) {
+        // source AR wider than target
+        // crop width to target AR
+        $wcrop0 = round($ar*$h0);
+        $hcrop0 = $h0;
+    } 
+    else {
+        // crop height to target AR
+        $wcrop0 = $w0;
+        $hcrop0 = round($w0/$ar);
+    }
+
+    // crop parameters for all scales and translations
+    $params = array();
+
+    // crop at different scales
+    $hgap = $hcrop0 - $h;
+    $hinc = ($nk == 1) ? 0 : $hgap / ($nk - 1);
+    $wgap = $wcrop0 - $w;
+    $winc = ($nk == 1) ? 0 : $wgap / ($nk - 1);
+
+    // find window with highest normalized edginess
+    $n = 10000;
+    $maxbetanorm = 0;
+
+    $maxparam = array('w'=>0, 'h'=>0, 'x'=>0, 'y'=>0);
+    for ($k = 0; $k < $nk; $k++) {
+        $hcrop = round($hcrop0 - $k*$hinc);
+        $wcrop = round($wcrop0 - $k*$winc);
+        $xcrop = $xcenter - $wcrop / 2;
+        $ycrop = $ycenter - $hcrop / 2;
+        //dprint("crop: $wcrop, $hcrop, $xcrop, $ycrop");
+
+        if ($xcrop < 0) $xcrop = 0;
+        if ($xcrop+$wcrop > $w0) $xcrop = $w0-$wcrop;
+        if ($ycrop < 0) $ycrop = 0;
+        if ($ycrop+$hcrop > $h0) $ycrop = $h0-$hcrop;
+
+        $beta = 0;
+        for ($c=0; $c<$n; $c++) {
+            $i = mt_rand(0,$wcrop-1);
+            $j = mt_rand(0,$hcrop-1);
+            $beta += imagecolorat($im, $xcrop+$i, $ycrop+$j) & 0xFF;
+        }
+        $area = $wcrop * $hcrop;
+        $betanorm = $beta / ($n*pow($area, $gamma-1));
+        //dprint("beta: $beta; betan: $betanorm");
+  
+        //dprint("image$k.jpg:<br/>\n<img src=\"$currfile\"/>");
+        // best image found, save it
+        if ($betanorm > $maxbetanorm) {
+            $maxbetanorm = $betanorm;
+            $maxparam['w'] = $wcrop;
+            $maxparam['h'] = $hcrop;
+            $maxparam['x'] = $xcrop;
+            $maxparam['y'] = $ycrop;
+            //$maxfile = $currfile;
+        }
+    }
+
+    // return image
+    $imgcp->cropImage($maxparam['w'],$maxparam['h'],
+                      $maxparam['x'],$maxparam['y']);
+    $imgcp->scaleImage($w,$h);
+    $imgcp->writeImage($out);
+
+    chmod($out, 0777);
+    $img->destroy();
+    $imgcp->destroy();
+    return 0;
+}
 
 
 
