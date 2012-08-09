c85bd614ab290c78ab818927bc3f6e940931d6f7
Vijay <vijay@hayek.(none)>
2012-08-09 16:47:51 -0700

    Add change for keeping track of the album owner for a given comment

--- database/Comments.sql
+++ database/Comments.sql
@@ -4,10 +4,12 @@ CREATE TABLE IF NOT EXISTS `Comments` (
   `comment` text NOT NULL,
   `albumphoto_id` int(11) unsigned NOT NULL,
   `album_id` int(11) unsigned NOT NULL,
+  `album_owner_id` int(11) unsigned NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY (`albumphoto_id`),
   KEY (`album_id`),
+  KEY (`album_owner_id`),
   KEY (`commenter_id`),
   KEY (`created`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
