11c13db040ea94e528f2b5b60f05411275047561
vijayb <vijayb@gmail.com>
2012-09-10 14:21:46 -0700

    Table that will allow event notifications to be created

---
+++ www/Events.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `Events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `actor_id` int(11) unsigned NOT NULL,
+  `action_type` int(11) unsigned NOT NULL,
+  `album_id` int(11) unsigned DEFAULT NULL,
+  `albumphoto_id` int(11) unsigned DEFAULT NULL,
+  `comment_id` int(11) unsigned DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY (`actor_id`),
+  KEY (`album_id`),
+  KEY (`albumphoto_id`),
+  KEY (`comment_id`)
+  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
