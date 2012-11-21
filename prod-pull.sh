94019b9324b444edd46149e93c9b075af9284a32
vijayb <vijayb@gmail.com>
2012-11-21 11:26:35 -0800

    Make prod-pull/test-pull work on both vijay and sanjay's machine by replacing /home/sanjay with $HOME

--- prod-pull.sh
+++ prod-pull.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ssh ubuntu@zipio.com 'cd /home/ubuntu/zipio; cp -r www www-live; cp -r notifier notifier-live; tar zcvf www-live.tar.gz www-live notifier-live; rm -rf www-live notifier-live;'
-scp ubuntu@zipio.com:/home/ubuntu/zipio/www-live.tar.gz /home/sanjay/zipio
+scp ubuntu@zipio.com:/home/ubuntu/zipio/www-live.tar.gz $HOME/zipio
 ssh ubuntu@zipio.com 'cd /home/ubuntu/zipio; rm -f www-live.tar.gz;'
 tar zxvf www-live.tar.gz;
 rm -f www-live.tar.gz;
--- test-pull.sh
+++ test-pull.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ssh ubuntu@zipiyo.com 'cd /home/ubuntu/zipio; cp -r www www-test; cp -r notifier notifier-test; tar zcvf www-test.tar.gz www-test notifier-test; rm -rf www-test notifier-test;'
-scp ubuntu@zipiyo.com:/home/ubuntu/zipio/www-test.tar.gz /home/sanjay/zipio
+scp ubuntu@zipiyo.com:/home/ubuntu/zipio/www-test.tar.gz $HOME/zipio
 ssh ubuntu@zipiyo.com 'cd /home/ubuntu/zipio; rm -f www-test.tar.gz;'
 tar zxvf www-test.tar.gz;
 rm -f www-test.tar.gz;
